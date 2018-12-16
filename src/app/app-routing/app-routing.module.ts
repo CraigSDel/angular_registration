ddc86c0f904ffff60a380b00b32ab95b8d8a5e6e
Craig Stroberg <strobergcd@gmail.com>
2018-12-16 19:04:56 +0200

    clean up

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {RegistrationComponent} from '../components/registration/registration.component';
 
-const routes: Routes = [
-  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  // { path: 'dashboard', component: DashboardComponent },
-  {path: 'registration', component: RegistrationComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import {DynamicFormComponent} from './dynamic-form/component/dynamic-form.component';
-import {DynamicFormQuestionComponent} from './dynamic-form/question/dynamic-form-question.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
-import {RegistrationComponent} from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DynamicFormComponent,
-    DynamicFormQuestionComponent,
-    RegistrationComponent
   ],
   imports: [
     BrowserModule,
@@ -23,5 +17,4 @@ import {RegistrationComponent} from './components/registration/registration.comp
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
