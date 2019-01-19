5f84b0cc1d883d8c200007f52f449ceb3953cc9a
Craig Stroberg <strobergcd@gmail.com>
2019-01-19 15:13:04 +0200

    Completed cross component state and angular material integration

---
+++ src/app/components/registration/registration-save/registration-save.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-save',
+  templateUrl: './registration-save.component.html',
+  styleUrls: ['./registration-save.component.css']
+})
+export class RegistrationSaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
